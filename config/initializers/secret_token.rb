6076d46248917adb0cfee0ba7bfe89da8b4fd711
dean <dean@ubuntu.(none)>
2012-09-27 11:59:52 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Milehigh::Application.config.secret_token = '836d2581a0e09455127e7be305ebcf78cfd481c93b03e3f1f83d3eecc1708114b10d317196f8d23525e47a6a65bbb87ca2fb61c078ceb5dc3715e2bcfddf4eb8'
